c29c40a83c1812b3eaf5266b54ecb96444e4a779
minecraft15611-Paul <minecraft15611@gmail.com>
2026-03-27 15:03:07 +0800

    CSS navbar, title, icon, searchbar, catagories buttons

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -57,23 +57,59 @@
 </script>
 
 <template>
-    <div class="product-list-container">
-        <h2>Products List</h2>
-        <div style="margin-bottom: 20px;">
-            <label>Max Price: ${{ maxPrice }}</label><br>
-            <input type="range" v-model.number="maxPrice" min="0" max="1000" step="50">
-            <br>
-            <input type="text" v-model="searchQuery" placeholder="Search products...">
+    <header>
+        <nav class="w-full border-b-3 border-gray-300 py-2">
+            <div class="container mx-auto flex justity-between ">
+                <div class="flex items-center space-x-3 w-1/3">
+                    <a href="#"><i class="fa-brands fa-facebook duration-300 transition hover:scale-110"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram duration-300 transition hover:scale-110"></i></a>
+                    <a href="#"><i class="fa-solid fa-magnifying-glass duration-300 transition hover:scale-110"></i></a>
+                </div>
+                <div class="w-1/3 text-center">
+                    <span class="flex justify-center text-sm text-gray-400 uppercase tracking-wide">Free Shipping This Week Order Over - $55</span>
+                </div>
+                <div class="flex justify-end w-1/3">
+                    <select name="language" id="language-select" class="flex justify-end">
+                        <option value="eng">ENGLISH</option>
+                        <option value="zh-Tw">繁體中文</option>
+                    </select>
+                </div>
+            </div>
+        </nav>
+        <div class="flex justify-between p-5 items-center border-b-3 border-gray-300">
+            <div class="flex  items-center w-1/4">
+                <h1 class="font-bold px-8 text-4xl">Lemon Tree</h1>
+            </div>
+            <div class="flex justify-center w-1/2">
+                <input type="text" v-model="searchQuery" placeholder="Enter your products name..." class="w-full border border-gray-300 rounded-lg px-3 py-2 self-center focus:outline-none focus:border-pink-400">
+                
+            </div>
+            <div class="flex justify-end w-1/4">
+                <div class="px-8 space-x-3 ">
+                    <i class="fa-regular fa-user text-2xl duration-300 transition hover:scale-110"></i>
+                    <i class="fa-regular fa-heart text-2xl duration-300 transition hover:scale-110"></i>
+                    <i class="fa-solid fa-bag-shopping text-2xl duration-300 transition hover:scale-110"></i>
+                </div>
+            </div>
         </div>
+    </header>
 
-        <div style="margin: 20px 0; display: flex; gap: 10px; justify-content: center; flex-wrap: wrap;">
+    <div class="product-list-container ">
+        <div class="flex justify-center gap-3 my-5 ">
             <button 
                 v-for="cat in categories" :key="cat" 
                 @click="setCategory(cat)"
                 :style="{ backgroundColor: selectedCategory === cat ? '#42b983' : '#eee', color: selectedCategory === cat ? 'white' : 'black' }"
+                class="font-semibold border-none px-3 rounded-md tracking-wide duration-300 transition hover:scale-110"
             >
                 {{ cat }}
             </button>
+        </div>
+        
+        <div style="margin-bottom: 20px;">
+            <label>Max Price: ${{ maxPrice }}</label><br>
+            <input type="range" v-model.number="maxPrice" min="0" max="1000" step="50">
+            <br>
         </div>
 
         <ul>
