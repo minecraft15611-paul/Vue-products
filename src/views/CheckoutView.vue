5fb788cda84529f88ed9ba29190db861efec6cb1
minecraft15611-Paul <minecraft15611@gmail.com>
2026-04-15 21:18:50 +0800

    checkout view page structure almost done now going for complete logic

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -26,6 +26,11 @@
     const toggle = () => {
         isOpen.value = !isOpen.value
     }
+
+    const isBottomOpen = ref<boolean>(false)
+    const bottomToggle = () => {
+        isBottomOpen.value = !isBottomOpen.value;
+    }
     
     const payment = ref<string>('credit');
     const useShippingAsBilling = ref<boolean>(true);
@@ -911,27 +916,275 @@
                     </p>
                 </div>
 
-                <div class="mt-10">
-                    <h3 class="text-[15px]">
-                        Carbon Offset
-                    </h3>
-                    <div class="flex">
-                        <p class="mt-2 text-gray-500 text-[13px]">
-                            Learn more about how we do this.
+                <div>
+                    <div class="mt-10">
+                        <h3 class="text-[15px]">
+                            Carbon Offset
+                        </h3>
+                        <div class="flex">
+                            <p class="mt-2 text-gray-500 text-[13px]">
+                                Learn more about how we do this.
+                            </p>
+                            <div>
+                                
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div class="flex border border-gray-300 p-2 mt-3">
+                        <input type="checkbox" name="" id="">
+                        <p class="text-[13px] pl-2">
+                            Offset CO2 emissions from shipping for $0.15
                         </p>
-                        <div>
-                            
+                    </div>
+                </div>
+
+                <Transition
+                    enter-active-class="overflow-hidden transition-[max-height,opacity] duration-500 ease-in-out"
+                    enter-from-class="max-h-0 opacity-0"
+                    enter-to-class="max-h-[500px] opacity-100"
+                    leave-active-class="overflow-hidden transition-[max-height,opacity] duration-300 ease-in-out"
+                    leave-from-class="max-h-[500px] opacity-100"
+                    leave-to-class="max-h-0 opacity-0"
+                >
+                    <div class="mb-3" v-if="isBottomOpen">
+                        <div  >
+                            <button class="p-2 border border-gray-200 mt-10 mb-3">
+                                Add discount
+                            </button>
+                        </div>
+
+                        
+                        <button 
+                            @click="bottomToggle"
+                            class="block w-full text-left appearance-none bg-transparent p-0 m-0 border-none outline-none group"
+                        >
+                            <div class="flex justify-between items-center gap-1"  >
+                                <div class="flex justify-start gap-3">
+                                    <div>
+                                        <div class="inline-block border border-gray-200 p-0.5 bg-white">
+                                            <img :src="cartStore.cart[0]?.img || '/placeholder.jpg'" class="w-10 h-10 object-cover bg-gray-100 flex-shrink:0" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        Total <br> 
+                                        <span class="text-[11px]">{{ cartStore.cart.reduce((acc, item) => acc + item.quantity, 0) }} items</span> 
+                                    </div>
+                                </div>
+                                <div class="flex justify-end gap-2 items-baseline">
+                                    <div class="flex justify-end text-[18px] font-bold">
+                                        ${{ subtotal.toFixed(2) }}
+                                    </div>
+                                    <div>
+                                        <svg 
+                                            :class="{'rotate-180': !isBottomOpen}"
+                                            class="w-4 h-4 transition-transform duration-300 ease-in-out text-gray-700 group-hover:text-gray-950" 
+                                            fill="none" 
+                                            viewBox="0 0 24 24" 
+                                            stroke="currentColor" 
+                                            stroke-width="2"
+                                        >
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                </Transition>
+
+
+
+                <!-- =============== Bottom Total Products List Accordion =============== -->
+
+                <Transition
+                    enter-active-class="overflow-hidden transition-[max-height,opacity] duration-500 ease-in-out"
+                    enter-from-class="max-h-0 opacity-0"
+                    enter-to-class="max-h-[500px] opacity-100"
+                    leave-active-class="overflow-hidden transition-[max-height,opacity] duration-300 ease-in-out"
+                    leave-from-class="max-h-[500px] opacity-100"
+                    leave-to-class="max-h-0 opacity-0"
+                >
+
+                    <div class="w-full max-w-md mt-10 rounded-lg overflow-hidden" v-if="!isBottomOpen">
+                        
+                        <div class="flex justify-between items-center mb-2">
+                            <button 
+                            @click="bottomToggle"
+                            class="w-full flex justify-between items-center  hover:bg-gray-50 transition-colors"
+                            >
+                                <span class="text-[20px] text-[#1a1a1a]  tracking-tight mr-2">
+                                    Order summary
+                                </span>
+            
+                                <svg 
+                                    :class="{'rotate-180': !isBottomOpen}"
+                                    class="w-4 h-4 transition-transform duration-300 ease-in-out text-gray-700 group-hover:text-gray-950" 
+                                    fill="none" 
+                                    viewBox="0 0 24 24" 
+                                    stroke="currentColor" 
+                                    stroke-width="2"
+                                >
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </button>
+                        </div>
+
+                        <!-- ========== Accordion Contents ========== -->
+
+
+                    
+                    
+                        <div >
+                            <div class=" text-gray-600 " >
+
+                                <div class="
+                                    flex-1 bg-white overflow-hidden  
+                                    overflow-y-auto max-h-[70vh] hide-scrollbar scrollbar-gutter:stable 
+                                    scroll-smooth 
+                                ">
+                                    <div
+                                        v-for="item in cartStore.cart"
+                                        :key="item.id"
+                                        class="flex items-start group gap-2 mb-2"
+                                    >
+                                        <!-- Thumbnail -->
+                                        <div class="inline-block border border-gray-200 p-0.5 bg-white relative ">
+                                            <img
+                                            :src="item.img || '/placeholder.jpg'"
+                                            :alt="item.title"
+                                            class="w-15 h-15  object-cover bg-gray-100 flex-shrink:0"
+                                            />
+                                            <div class="absolute top-0 -right-1 bg-black text-white text-xs w-5 h-5 flex items-center justify-center rounded-md">
+                                                {{ item.quantity }}
+                                            </div>
+                                        </div>
+
+                                        <!-- Info -->
+                                        <div class="flex-1 min-w-0">
+                                            <div class="flex w-45">
+                                                <p class="font-light leading-relaxed text-[13px] text-gray-900">
+                                                    {{ item.title }} | Navy
+                                                </p>
+                                            </div>
+
+                                        </div>
+
+                                        <div class="">
+                                            <p class="flex items-end font-sans text-[13px] font-medium text-gray-900 tracking-tight tabular-nums">
+                                                ${{ (item.price*item.quantity).toFixed(2) }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="flex justify-between mt-2 mb-1 gap-2">
+                                <div class="relative border border-gray-300 focus-within:ring-2 focus-within:ring-black">
+                                    <input type="text" id="firstname" name="firstname" 
+                                            placeholder=" " autocomplete="given-name"
+                                            v-model="discountcode"
+                                        class="peer w-62 h-11 border text-[13px] border-gray-100 pt-5 pb-1 px-3 text-black focus-within:ring-2 focus-within:ring-black bg-transparent"
+                                    >
+                                    <label 
+                                        for="firstname" 
+                                        class="absolute left-3 top-3 text-[13px] text-gray-400 transition-all duration-200 pointer-events-none
+                                        peer-focus:top-1 
+                                        peer-focus:text-xs 
+                                        peer-focus:text-gray-800 
+                                        peer-[:not(:placeholder-shown)]:top-1 
+                                        peer-[:not(:placeholder-shown)]:text-xs 
+                                        peer-[:not(:placeholder-shown)]:text-gray-500"
+                                    >
+                                        Discount code or gift card
+                                    </label>
+                                </div>
+                                <button class="border text-[13px] transition-color duration-300 border-gray-300 text-gray-500 px-2"
+                                        :disabled="isApplyDisabled"
+                                        :class="isApplyDisabled ? 'bg-gray-50 ' : 'bg-black text-white'"
+                                >
+                                    APPLY
+                                </button>
+                            </div>
+
+                            <div class="flex justify-between mt-5 mb-2">
+                                <div class="text-[13px] ">
+                                    Subtotal - {{ cartStore.cart.reduce((acc, item) => acc + item.quantity, 0) }} items
+                                </div>
+                                <div class="">
+                                    ${{ subtotal.toFixed(2) }}
+                                </div>
+                            </div>
+
+                            <div class="flex justify-between">
+                                <div class="text-[13px] ">
+                                    Shipping 
+                                </div>
+                                <div class="">
+                                    Free
+                                </div>
+                            </div>
+
+                            <div class="flex justify-between my-5">
+                                <div class=" text-[20px]">
+                                    Total
+                                </div>
+                                <div class="">
+                                    ${{ total.toFixed(2) }}
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </Transition>
+
+                                    <!-- ========== PAY NOW BUTTON ========== -->
+
+                <div class="flex-grid ">
+                    <div class="w-full">
+                        <button class="flex w-full justify-center items-center bg-black text-white text-[15px] py-3 ">
+                            PAY NOW
+                        </button>
+                    </div>
+                    <div class="flex p-5 gap-3 bg-gray-100 border border-gray-200 mt-4">
+                        <div class="flex">
+                            <div class="mr-3">
+                               <svg 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    fill="none" 
+                                    viewBox="0 0 24 24" 
+                                    stroke-width="1.5" 
+                                    stroke="currentColor" 
+                                    class="w-5 h-5 text-gray-400"
+                                >
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                                </svg>
+                            </div>
+                            <div>
+                                <p class="text-[13px] text-gray-700 leading-normal">
+                                    The total amount you pay includes all applicable customs duties & taxes. 
+                                    We guarantee no additional charges on delivery.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="flex border border-gray-300 p-2 mt-3">
-                    <input type="checkbox" name="" id="">
-                    <p class="text-[13px] pl-2">
-                        Offset CO2 emissions from shipping for $0.15
-                    </p>
-                </div>
-                
+
+            </div>
+        </div>
+
+        <div class="border border-gray-300 "></div>
+
+        <div class="flex-grid justify-between my-5">
+            <div class="flex gap-5 justify-center text-[13px] underline">
+                <div>Refund Policy.</div>
+                <div>Privacy Policy.</div>
+            </div>
+            <div class="flex gap-5 justify-center text-[13px] underline">
+                <div>Terms of Service</div>
+                <div>Contact</div>
             </div>
         </div>
 
