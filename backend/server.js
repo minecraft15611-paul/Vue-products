cc3714ee8342ed27b9aa6e297f681cd8e4f8d19e
minecraft15611-Paul <minecraft15611@gmail.com>
2026-05-12 19:08:12 +0800

    test upload

--- backend/server.js
+++ backend/server.js
@@ -48,25 +48,25 @@ app.use(express.json()); // 解析前端傳來的 JSON 資料
 // 3. 路由設定 (Routes)
 // [GET] 獲取所有商品
 app.get('/api/products', async (req, res) => {
-  try {
-    const dbProducts = await Product.find(); // 向資料庫要所有商品
-    res.json(dbProducts);
-  } catch (err) {
-    res.status(500).json({ message: "抓取失敗", error: err });
-  }
+    try {
+        const dbProducts = await Product.find(); // 向資料庫要所有商品
+        res.json(dbProducts);
+    } catch (err) {
+        res.status(500).json({ message: "抓取失敗", error: err });
+    }
 });
 
 // [POST] 接收 Vue 上傳的新商品
 app.post('/api/products', async (req, res) => {
-  try {
-    const newProduct = new Product(req.body); // 建立新商品物件
-    const savedProduct = await newProduct.save(); // 存進資料庫
-    res.status(201).json(savedProduct);
-  } catch (err) {
-    res.status(400).json({ message: "新增失敗", error: err });
-  }
+    try {
+        const newProduct = new Product(req.body); // 建立新商品物件
+        const savedProduct = await newProduct.save(); // 存進資料庫
+        res.status(201).json(savedProduct);
+    } catch (err) {
+        res.status(400).json({ message: "新增失敗", error: err });
+    }
 });
-
+ 
     // [DELETE] 刪除指定商品
     app.delete('/api/products/:id', async (req, res) => {
     try {
