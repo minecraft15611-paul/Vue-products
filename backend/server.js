4cb6b72523f50027b39ba4ca9b67255652939e24
minecraft15611-Paul <minecraft15611@gmail.com>
2026-05-30 21:57:34 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -189,7 +189,7 @@ app.use(cors({
         'http://localhost:5173',
         'http://127.0.0.1:5173',
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 }));
